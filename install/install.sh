be0425dd15687a6f0eb84da4969f2e4d25d85cdc
Jakub Bujny <jakub.bujny@karhoo.com>
2021-02-02 10:38:39 +0100

    Add installation via ansible/docker

--- install/install.sh
+++ install/install.sh
@@ -4,4 +4,13 @@ apt-get install -y ansible
 
 ansible-galaxy install geerlingguy.docker_arm
 
+cd /tmp
+
+wget https://raw.githubusercontent.com/jakubbujny/smoggler/main/install/ansible-local.yaml
+
+ansible-playbook ansible-local.yaml
+
+cd /opt/smoggler
+
+docker-compose up -d
 
