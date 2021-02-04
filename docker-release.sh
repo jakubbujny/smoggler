a7ae534801e35c76c6efd9fb9e571f748986475f
Jakub Bujny <jakub.bujny@karhoo.com>
2021-02-04 09:19:23 +0100

    fix styling

--- docker-release.sh
+++ docker-release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t jakubbujny/smoggler:0.0.5 -t jakubbujny/smoggler:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t jakubbujny/smoggler:0.0.6 -t jakubbujny/smoggler:latest --push .
