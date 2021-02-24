f202dcad0ed0ac06a509a7aeadcc4cdb9584283d
Jakub Bujny <jakub.bujny@karhoo.com>
2021-02-24 13:32:58 +0100

    fix version

--- docker-release.sh
+++ docker-release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t jakubbujny/smoggler:${VERSION} -t jakubbujny/smoggler:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t jakubbujny/smoggler:${VERSION} --push .
