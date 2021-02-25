b0ca9cf401ef50b2de0d4c557cc4b9a6c80fe3ca
Jakub Bujny <jakub.bujny@karhoo.com>
2021-02-25 14:05:17 +0100

    release 0.0.7

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-export VERSION=0.0.7-rc9
+export VERSION=0.0.7
 
 DEV=${DEV:-"false"}
 
@@ -25,9 +25,9 @@ cd /opt/smoggler
 
 if [ "$DEV" = "true" ]; then
   echo "DEV=true" > .env
-  echo "VERSION=0.0.7-rc9
+  echo "VERSION=0.0.7
 else
-  echo "VERSION=0.0.7-rc9
+  echo "VERSION=0.0.7
 fi
 
 if [ "$DEV" = "true" ]; then
